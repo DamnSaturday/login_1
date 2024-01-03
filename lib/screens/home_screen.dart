452e65bc90aabcc5de01e15c4b221068fe7922e4
maderobills <dennisbortey60@gmail.com>
2024-01-03 13:33:47 +0000

    new .0.01.03 b_nav_b onTap done

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Enable full-screen mode
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive,
+        overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top]);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Student Portal'),
+        backgroundColor: Colors.transparent,
+        // You can add actions or other app bar properties here
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logo.png',
+                height: 100,
+                width: 100,
+              ),
+              const SizedBox(height: 40),
+              const Text(
+                'Home',
+                style: TextStyle(
+                  fontSize: 45.0,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xFF821538),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/b_nav.dart
+++ lib/widgets/b_nav.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:login/screens/home_screen.dart';
+import 'package:login/screens/welc_screen.dart';
 
 class BottomNavigation extends StatefulWidget {
   const BottomNavigation({Key? key}) : super(key: key);
@@ -9,37 +11,107 @@ class BottomNavigation extends StatefulWidget {
 }
 
 class _BottomNavigationState extends State<BottomNavigation> {
+  int _currentIndex = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.transparent,
-      bottomNavigationBar: Container(
-        color: const Color(0xFF821538),
-        child: const Padding(
-          padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-          child: GNav(
-            backgroundColor: Color(0xFF821538),
-            color: Colors.white,
-            activeColor: Color(0xFF821538),
-            tabBackgroundColor: Colors.white,
-            padding: EdgeInsets.all(10),
-            gap: 8,
-            tabs: [
-              GButton(
-                icon: Icons.home,
-                text: 'Home',
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          children: [
+            Container(
+              color: Colors.transparent,
+              child: const Center(
+                child: HomeScreen(),
               ),
-              GButton(
+            ),
+            Container(
+              color: Colors.red,
+              child: const Center(
+                child: Text('Page 2'),
+              ),
+            ),
+            Container(
+              color: Colors.greenAccent,
+              child: const Center(
+                child: Text('Page 3'),
+              ),
+            ),
+            Container(
+              color: Colors.yellow,
+              child: const Center(
+                child: Text('Page 4'),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFF821538),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(0.1),
+            ),
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+              gap: 8,
+              activeColor: const Color(0xFF821538),
+              iconSize: 24,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              duration: const Duration(milliseconds: 400),
+              tabBackgroundColor: Colors.white,
+              tabs: const [
+                GButton(
+                  icon: Icons.home,
+                  text: 'Home',
+                ),
+                GButton(
                   icon: Icons.favorite_border,
-                  text: 'Likes',),
-              GButton(
-                icon: Icons.search,
-                text: 'Search',),
-              GButton(
-                icon: Icons.settings,
-                text: 'Settings',),
-            ],
+                  text: 'Likes',
+                ),
+                GButton(
+                  icon: Icons.search,
+                  text: 'Search',
+                ),
+                GButton(
+                  icon: Icons.settings,
+                  text: 'Settings',
+                ),
+              ],
+              selectedIndex: _currentIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _currentIndex = index;
+                  _pageController.jumpToPage(index);
+                });
+              },
+            ),
           ),
         ),
       ),
